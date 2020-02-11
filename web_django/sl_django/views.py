2db3d2d48d1e0c6987a8267308eb4b21fafc9448
Lukashenkov, Vyacheslav <Vyacheslav.Lukashenkov@emc.com>
2020-02-11 23:35:52 +0300

    some views work

--- web_django/sl_django/views.py
+++ web_django/sl_django/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
 
 def sl_django(request):
 	return render(request, 'sl_django.html', {})
 
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
